b4559449e270aa140516eabc9d9dcc0e98b89251
Lens Desrouleaux <65369396+lrdesrouleaux@users.noreply.github.com>
2021-02-02 11:44:51 -0500

    Update classify.py
    added an infinite loop through a new directory and commented on some code

--- classify.py
+++ classify.py
@@ -5,34 +5,53 @@ import argparse
 import imutils
 import pickle
 import cv2 as cv
-import os
+import os, random
 ap=argparse.ArgumentParser()
+#model location
 ap.add_argument("-m", "--model",default="vggbird.model" ,required=False,
 	help="path to trained model model")
+#label locations
 ap.add_argument("-l", "--labelbin",default="lb.pickle" ,required=False,
 	help="path to label binarizer")
-ap.add_argument("-i", "--image",default="/Users/lens/Downloads/f.webp" ,
+#image locations
+ap.add_argument("-i", "--imagepath",default="/Users/lens/Documents/CVproject/dataset/classifyimages" ,
             required=False,
 	help="path to input image")
 args = vars(ap.parse_args())
-image=cv.imread(args["image"])
-output=image.copy()
-#change image to model spec
-image=cv.resize(image,(96,96))
-image=image.astype("float")/255.0
-image=img_to_array(image)
-image=np.expand_dims(image,axis=0)
-print("<info> loading model...")
-model=load_model(args["model"])
-lb=pickle.loads(open(args["labelbin"],"rb").read())
-print("<info> classifying image")
-proba=model.predict(image)[0]
-idx=np.argmax(proba)
-label=lb.classes_[idx]
-filename = args["image"][args["image"].rfind(os.path.sep) + 1:]
-output = imutils.resize(output, width=400)
-cv.putText(output, label, (10, 25),  cv.FONT_HERSHEY_SIMPLEX,
-	0.7, (0, 255, 0), 2)
-print("[INFO] {}".format(label))
-cv.imshow("Output", output)
-cv.waitKey(0)
+#set the files to a list
+files=os.listdir(args["imagepath"])
+while(True):
+    #pick a random index from that list
+	index=random.randrange(0,len(files)+1)
+	print(files[index])
+	#set it to a a string
+	imagefile=args["imagepath"]+"/"+files[index]
+	print(imagefile)
+	image=cv.imread(imagefile)
+	if image is None:
+		print("image error trying to grab:",imagefile)
+		break
+	output=image.copy()
+	#change image to model spec
+	image=cv.resize(image,(96,96))
+	image=image.astype("float")/255.0
+	#convert the image to a 3d numpy array
+	image=img_to_array(image)
+	#expand the array to make it 3 dimensions
+	image=np.expand_dims(image,axis=0)
+	#run the model
+	print("<info> loading model...")
+	model=load_model(args["model"])
+	lb=pickle.loads(open(args["labelbin"],"rb").read())
+	print("<info> classifying image")
+	proba=model.predict(image)[0]
+	idx=np.argmax(proba)
+	label=lb.classes_[idx]
+	filename = files[index][files[index].rfind(os.path.sep) + 1:]
+	output = imutils.resize(output, width=400)
+	cv.putText(output, label, (10, 25),  cv.FONT_HERSHEY_COMPLEX,
+		0.7, (0, 255, 0), 2)
+	print("[INFO] {}".format(label))
+	cv.imshow("Output", output)
+	cv.waitKey(0)
+	cv.destroyAllWindows()
